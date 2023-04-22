153f81684413b59a223fed7d6fd152b702dfbd29
arigm7 <arisbethgm12@gmail.com>
2023-04-21 20:14:17 -0600

    Catalogo y movimiento

---
+++ src/ganaderiafx/controlador/CatalogoController.java
@@ -0,0 +1,114 @@
+
+package ganaderiafx.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+
+public class CatalogoController implements Initializable {
+
+    @FXML
+    private Pane pnl_categoria;
+    @FXML
+    private Pane pnl_busqueda;
+    @FXML
+    private TextField txt_buscar;
+    @FXML
+    private Button btn_buscarCatalogo;
+    @FXML
+    private Button btn_limpiarCatalogo;
+    @FXML
+    private Label lbl_nombreUsuario;
+    @FXML
+    private Button btn_nuevaCatalogo;
+    @FXML
+    private Button btn_editarCatalogo;
+    @FXML
+    private Button btn_activarCatalogo;
+    @FXML
+    private Button btn_desactivarCatalogo;
+    @FXML
+    private TableView<?> tbl_catalogo;
+    @FXML
+    private TableColumn<?, ?> tcl_idCatalogo;
+    @FXML
+    private TableColumn<?, ?> tcl_catalogo;
+    @FXML
+    private TableColumn<?, ?> tcl_conceptoCatalogo;
+    @FXML
+    private TableColumn<?, ?> tbl_estatusCatalogo;
+    @FXML
+    private Button btn_nuevaRol;
+    @FXML
+    private Button btn_editarRol;
+    @FXML
+    private Button btn_activarRol;
+    @FXML
+    private Button btn_desactivarRol;
+    @FXML
+    private TableView<?> tbl_rol;
+    @FXML
+    private TableColumn<?, ?> tcl_idRol;
+    @FXML
+    private TableColumn<?, ?> tcl_nombre;
+    @FXML
+    private TableColumn<?, ?> tcl_estatus;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void buscarCodigo(ActionEvent event) {
+    }
+
+    @FXML
+    private void limpiar(ActionEvent event) {
+    }
+
+    @FXML
+    private void nuevaCatalogo(ActionEvent event) {
+    }
+
+    @FXML
+    private void editarCatalogo(ActionEvent event) {
+    }
+
+    @FXML
+    private void activarCatalogo(ActionEvent event) {
+    }
+
+    @FXML
+    private void desactivarCatalogo(ActionEvent event) {
+    }
+
+    @FXML
+    private void nuevaRol(ActionEvent event) {
+    }
+
+    @FXML
+    private void editarRol(ActionEvent event) {
+    }
+
+    @FXML
+    private void activarRol(ActionEvent event) {
+    }
+
+    @FXML
+    private void desactivarRol(ActionEvent event) {
+    }
+    
+}
---
+++ src/ganaderiafx/controlador/MovimientoController.java
@@ -0,0 +1,124 @@
+
+package ganaderiafx.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+
+public class MovimientoController implements Initializable {
+
+    @FXML
+    private Pane pnl_busqueda;
+    @FXML
+    private TextField txt_buscarMovimiento;
+    @FXML
+    private Button btn_buscarMovimiento;
+    @FXML
+    private Button btn_limpiarMovimiento;
+    @FXML
+    private Label lbl_nombreUsuario;
+    @FXML
+    private Button btn_nuevaIngreso;
+    @FXML
+    private Button btn_editarIngreso;
+    @FXML
+    private Button btn_activarIngreso;
+    @FXML
+    private Button btn_desactivarIngresos;
+    @FXML
+    private TableView<?> tbl_ingresos;
+    @FXML
+    private TableColumn<?, ?> tcl_idIngreso;
+    @FXML
+    private TableColumn<?, ?> tcl_cantidadIngreso;
+    @FXML
+    private TableColumn<?, ?> tcl_observacionesIngreso;
+    @FXML
+    private TableColumn<?, ?> tcl_fechaCreacionIngreso;
+    @FXML
+    private TableColumn<?, ?> tcl_fechaModificacionIngreso;
+    @FXML
+    private TableColumn<?, ?> tcl_catalogoIngreso;
+    @FXML
+    private TableColumn<?, ?> tcl_conceptoIngreso;
+    @FXML
+    private Button btn_nuevaEgreso;
+    @FXML
+    private Button btn_editarEgreso;
+    @FXML
+    private Button btn_activarEgreso;
+    @FXML
+    private Button btn_desactivarEgreso;
+    @FXML
+    private TableView<?> tbl_egreso;
+    @FXML
+    private TableColumn<?, ?> tcl_idEgreso;
+    @FXML
+    private TableColumn<?, ?> tcl_motivoEgreso;
+    @FXML
+    private TableColumn<?, ?> tcl_observacionesEgreso;
+    @FXML
+    private TableColumn<?, ?> tcl_fechaCreacionEgreso;
+    @FXML
+    private TableColumn<?, ?> tcl_fechaMotificacionEgreso;
+    @FXML
+    private TableColumn<?, ?> tcl_conceptoEgreso;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void buscarMovimiento(ActionEvent event) {
+    }
+
+    @FXML
+    private void limpiarMovimiento(ActionEvent event) {
+    }
+
+    @FXML
+    private void nuevaIngreso(ActionEvent event) {
+    }
+
+    @FXML
+    private void editarIngreso(ActionEvent event) {
+    }
+
+    @FXML
+    private void activarIngreso(ActionEvent event) {
+    }
+
+    @FXML
+    private void desactivarIngresos(ActionEvent event) {
+    }
+
+    @FXML
+    private void nuevaEgreso(ActionEvent event) {
+    }
+
+    @FXML
+    private void editarEgreso(ActionEvent event) {
+    }
+
+    @FXML
+    private void activarEgreso(ActionEvent event) {
+    }
+
+    @FXML
+    private void desactivarEgreso(ActionEvent event) {
+    }
+    
+}
